da3b4e1994dc1cffe49288fccd89b0d362d65598
Cleyton Rodrigues <88549697+cleytonsilvadev@users.noreply.github.com>
2022-05-13 10:36:42 -0300

    if e elif em repetição
     printando o numero por extenso

---
+++ if e elif em repetição.py
@@ -0,0 +1,18 @@
+contador = 0
+
+while contador<=5:
+ contador=contador +1
+ if contador == 1:
+  print('Este numero é um')
+ elif contador == 2:
+  print('Este numero é dois')
+ elif contador == 3:
+  print('Este numero é tres')
+ elif contador == 4:
+  print('Este numero é quatro')
+ elif contador == 5:
+  print('Este numero é cinco')
+ else:
+   print('parametro errado')
+ print(contador)
+ 
